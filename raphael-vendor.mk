859a0839f9789f8cab14d0e323e0675655ff9d01
kondors1995 <normandija1945@gmail.com>
2025-07-27 10:56:16 +0000

    raphael: update hotword blobs from nabu OS1.0.18.0.TKXINXM

--- raphael-vendor.mk
+++ raphael-vendor.mk
@@ -878,8 +878,8 @@ PRODUCT_PACKAGES += \
     IWlanService \
     SoterService \
     TimeService \
-    HotwordEnrollmentOKGoogleHEXAGON \
-    HotwordEnrollmentXGoogleHEXAGON \
+    HotwordEnrollmentOKGoogleHEXAGON_WIDEBAND \
+    HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
     DeviceInfo \
     ImsRcsService \
     PowerOffAlarm \
